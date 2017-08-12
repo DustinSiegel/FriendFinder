79eb28f2bacd01985f8cc4954d2dbdf5f985119a
Dustin J Siegel <weenzombie@gmail.com>
2017-08-12 00:49:38 -0600

    console.logs climberData array

---
+++ app/data/friends.js
@@ -0,0 +1,23 @@
+// An array to collect survey answers =====================================================================================
+var climberData = [
+	{
+		"name":"Dustin Siegel",
+		"photo":"https://drive.google.com/open?id=0B5WxQUw0pX3WTDFhUmw0bG1PbzA",
+		"scores":[
+			2,
+			2,
+			2,
+			5,
+			2,
+			4,
+			4,
+			2,
+			3,
+			4,
+			5,
+			4,
+		]
+	}
+]
+
+module.exports = climberData;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,17 +1,17 @@
 // Links to a source file with an array of friendsData ===================================================================
-var friendsData = require("../data/friends");
+var climberData = require("../data/friends");
 
 // Sets up a routing function that connects to an html file ==============================================================
 module.exports = function(app) {
 
 // Connects to an array on a javascript file and gets any data ===========================================================
 	app.get("/api/friends", function(req, res) {
-    	res.json(friendsData);
+    	res.json(climberData);
   	});
 
 // Takes in user input data, converts it to json and posts it to the array  ==============================================
 	app.post("/api/friends", function(req, res) {
-		friendsData.push(req.body);
-		console.log(friendsData);
+		climberData.push(req.body);
+		console.log(climberData);
 	});
 };
