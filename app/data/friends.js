3637e9a3761ef2227bf8b37599ffd74b13196e06
Dustin J Siegel <weenzombie@gmail.com>
2017-09-06 20:36:23 -0600

    It now creates a value for the results of the servay but does not yet compare it to the ones in the API

--- app/data/friends.js
+++ app/data/friends.js
@@ -17,6 +17,60 @@ var climberData = [
 			5,
 			4,
 		]
+	},
+	{
+		"name":"Ross Primm",
+		"photo":"https://drive.google.com/open?id=0B5WxQUw0pX3WMHk1SVl5Z0FrVVk",
+		"scores":[
+			2,
+			1,
+			1,
+			5,
+			1,
+			2,
+			3,
+			1,
+			2,
+			2,
+			1,
+			5,
+		]
+	},
+	{
+		"name":"Quiola (Sam) Quinones",
+		"photo":"https://drive.google.com/open?id=0B5WxQUw0pX3WUUsyN0poYW1kaXc",
+		"scores":[
+			3,
+			1,
+			2,
+			5,
+			1,
+			5,
+			3,
+			1,
+			3,
+			3,
+			3,
+			5,
+		]
+	},
+	{
+		"name":"John Maestas",
+		"photo":"https://drive.google.com/open?id=0B5WxQUw0pX3WdzAtd1pzLWoxZHM",
+		"scores":[
+			3,
+			1,
+			4,
+			4,
+			3,
+			5,
+			4,
+			2,
+			3,
+			4,
+			2,
+			5,
+		]
 	}
 ]
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,23 +7,53 @@ module.exports = function(app) {
 // Connects to an array on a javascript file and gets any data ===========================================================
 	app.get("/api/friends", function(req, res) {
     	res.json(climberData);
+    	console.log("this is working");
   	});
 
 // Takes in user input data, converts it to json and posts it to the array  ==============================================
 	app.post("/api/friends", function(req, res) {
-
+		console.log(req.body, "req.body");
 // Compare the user input to the existing profiles to find the best match ================================================		
 // Not yet working !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!		
-	// 	var bestMatch = {
-	// 	name: "",
-	// 	phot: "",
-	// 	friendDifference: Infinity
-	// };
-	// 	//create an empty variable for matches
-	// 	var userData = req.body;
-	// 	var userScores = userData.scores;
-	// 	//create a forloop  that loops through the new user array and compares to the total data array.
+		var bestMatch = {
+		name: "",
+		photo: "",
+		friendDifference: Infinity
+	};
+		//create an empty variable for matches
+		var userData = req.body;
+		var userScores = userData.scores;
+		var climberDataScores = climberData[0].scores;
+		var scoreDifference = [];
+		var resultsArray = [];
+		// var finalUserScores = parseInt(userScores);
 
+		for (var i = 0; i < userScores.length; i++) {
+			scoreDifference.push( climberDataScores[i] - userScores[parseInt(i)]);
+		}
+		console.log("Score Difference", scoreDifference);
+		resultsArray = scoreDifference.reduce(function (a,b) {
+			return a + b
+		}, 0);
+
+
+		// console.log("ClimberData", climberData);
+		// console.log("User Data Scores", userData.scores);
+		// console.log("ClimberDataScores", climberDataScores);
+		// console.log("userScores", userScores);
+		// console.log("finalUserScores", finalUserScores);
+		console.log(resultsArray);
+		return resultsArray;
+
+
+
+
+
+
+
+	// // 	//create a forloop  that loops through the new user array and compares to the total data array.
+	// 	alert(userData);
+	// 	alert(userScores);
 	// 	//calcuate the difference 
 	// 	totalDifference <= Math.abs(parsInt) //the rest of it too.
 
