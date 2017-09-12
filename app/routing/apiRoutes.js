e5ab84b06ca33a63ae0eef3183214df43828e3a0
Dustin J Siegel <weenzombie@gmail.com>
2017-09-12 13:25:12 -0600

    the api now calculates the closest match and displays it as a variable

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,39 +1,39 @@
-// Links to a source file with an array of friendsData ===================================================================
+// Links to a source file with an array of friendsData =========================================================================
 var climberData = require("../data/friends");
 
-// Sets up a routing function that connects to an html file ==============================================================
+// Sets up a routing function that connects to an html file ====================================================================
 module.exports = function(app) {
 
-// Connects to an array on a javascript file and gets any data ===========================================================
+// Connects to an array on a javascript file and gets any data =================================================================
 	app.get("/api/friends", function(req, res) {
     	res.json(climberData);
     	console.log("this is working");
   	});
 
-// Takes in user input data, converts it to json and posts it to the array  ==============================================
+// Takes in user input data, converts it to json and posts it to the array  ====================================================
 	app.post("/api/friends", function(req, res) {
 		console.log("req.body", req.body);
 		console.log("ClimberData", climberData);
 
-// Compare the user input to the existing profiles to find the best match ================================================		
+// Compare the user input to the existing profiles to find the best match ======================================================		
 
-// Variables for the data from the user input and another variable for just the scores from that data ====================
+// Variables for the data from the user input and another variable for just the scores from that data ==========================
 		var userData = req.body;
 		var currentClimberScores = userData.scores;
 
-// A variable that takes the data from the friends.js and puts it into an array and seperates out the scores =============	
+// A variable that takes the data from the friends.js and puts it into an array and seperates out the scores ===================	
 		var allClimberScores = climberData.map(function(currentClimber) {
 			return currentClimber.scores;
 		});
 
-// A variable that takes the array of climber scores and compares it to the array of scores ==============================
-// from the user and puts the value of the difference between the two into a third array =================================
+// A variable that takes the array of climber scores and compares it to the array of scores ====================================
+// from the user and puts the value of the difference between the two into a third array =======================================
 		var allDifferences = allClimberScores.map(function(otherClimberScores) {
 			return getClimberScoreDifference(currentClimberScores, otherClimberScores)
 		});
 
-// A varaible that takes the array of the difference in scores between the user and all climbers and reduces each set ====
-// to a single number by adding them together ============================================================================ 
+// A varaible that takes the array of the difference in scores between the user and all climbers and reduces each set ==========
+// to a single number by adding them together ================================================================================== 
 		var collapsedDifferences = allDifferences.map(function(currentDifferenceScores) {
 			return currentDifferenceScores.reduce(function(accumulator, currentScore) {
 				return accumulator + currentScore;
@@ -41,31 +41,26 @@ module.exports = function(app) {
 			
 		});
 
-// A varaible that takes the reduced numbers variable and makes them positive numbers ===================================
+// A varaible that takes the reduced numbers variable and makes them positive numbers ==========================================
 		var absoluteDifferences = collapsedDifferences.map(function(currentDifference) {
 			return Math.abs(currentDifference);
 		});
 
-		// Calculate the minimum difference - the id of this score corresponds to the matched climber!
+// A variable that take the absolute difference array and finds the smallest number and points to its position in the array ====
+		var mostCompatableArrayPosition = absoluteDifferences.indexOf(Math.min.apply(null, absoluteDifferences));
 
-		
 		console.log("allClimberScores: ", allClimberScores);
 		console.log("allDifferences:", allDifferences);
 		console.log('collapsedDifferences:', collapsedDifferences);
-		console.log("abosoluteDifferences:", absoluteDifferences);
+		console.log("absoluteDifferences:", absoluteDifferences);
+		console.log("mostCompatableArrayPosition:", mostCompatableArrayPosition);
+		console.log("Match:", climberData[mostCompatableArrayPosition]);
 
-		// var resultsArray = [];
-		// resultsArray = climberDifferenceScores.reduce(function (accumulator, currentItem) {
-		// 	return accumulator + currentItem;
-		// }, 0);
-
-		// console.log("Difference value:", resultsArray);
-		// return resultsArray;
-
-		// Push the result back to the modal ====================================================================================
+// Push the result back to the modal ===========================================================================================
 		climberData.push(req.body);
 		res.json(true);
 
+// A function that does the work of comparing the climber scores and producing an array of the difference between each answer ===
 		function getClimberScoreDifference(climberOneScores, climberTwoScores) {
 			var scoreDifferences = [];
 
